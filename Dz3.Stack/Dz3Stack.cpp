86a9665f27a878515d1f6edc90f61a0cb1356e00
Phil <joy770@rambler.ru>
2017-10-20 23:14:30 +0300

    Добавьте файлы проекта.

---
+++ Dz3.Stack/Dz3Stack.cpp
@@ -0,0 +1,53 @@
+// Dz3Stack.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define STACKSIZE 10
+struct STR // структура данных в стеке
+{
+	char FIO[100];
+	int tel;
+	struct STR *pSTR;
+};
+struct STR STACK[STACKSIZE]; // стек
+struct STR* STACKPOINTER = NULL;// указатель на вершину стека
+struct STR data; // текущая информация
+void push()// добавление в стек нового элемента
+{
+	data.pSTR = STACKPOINTER;
+	STACKPOINTER = (struct STR*)malloc(sizeof(struct STR)); // указатель на выделенную память
+	printf("Enter the data for the stack:\n");
+	scanf("%s\n%d",&data.FIO,&data.tel);
+	*STACKPOINTER = data;// запись в выделеную память данных
+
+
+}
+void pop() // удаление из стека
+{
+	STACKPOINTER  = data.pSTR;
+}
+void peek()// чтение головного элемента
+{
+	if (STACKPOINTER != NULL)
+	{
+		data = *STACKPOINTER;
+		printf("FIO: %s | Tel: %d",data.FIO,data.tel);
+	}
+	else
+	{
+		printf("STACK IS EMPTY!");
+	}
+}
+int main()
+{
+
+
+	push();
+	pop();
+	peek();
+	getchar();
+	getchar();
+    return 0;
+}
+
